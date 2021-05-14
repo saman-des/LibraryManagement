d2d1a570415d2e4d501d579589c693952d29d794
saman <samantamrakar94@gmail.com>
2021-05-15 00:12:53 +0545

    third commit

--- crm1/settings.py
+++ crm1/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'LMS',
+        # 'NAME': 'LMS',
         'USER': 'postgres',
         'PASSWORD': 'subin123',
         'HOST': 'localhost',
